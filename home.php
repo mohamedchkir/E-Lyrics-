2ad207139438d0b4bec857099096a3cdfc9ed4e7
mohamed chkir <mohamedchkir2@gmail.com>
2023-01-23 20:49:55 +0100

    travailler sur style home

--- home.php
+++ home.php
@@ -71,13 +71,13 @@ $stat = new Music();
                         <!--Grid starts here-->
                         <div class="flex items-center justify-between p-5 bg-amber-300  rounded shadow-sm">
                             <div>
-                                <div class="text-sm text-gray-400 ">Music</div>
+                                <div class="font-black text-xl text-gray-900 ">Music</div>
                                 <div class="flex items-center pt-1">
-                                    <div class="text-3xl font-medium text-gray-600 "><?php echo $stat->countMusic()['COUNT(id)'] ?></div>
+                                    <div class="text-3xl font-medium text-gray-50 "><?php echo $stat->countMusic()['COUNT(id)'] ?></div>
                                 </div>
                             </div>
                             <div class="text-gray-500 text-4xl">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9 text-gray-50">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
                                 </svg>
 
@@ -87,13 +87,13 @@ $stat = new Music();
 
                         <div class="flex items-center justify-between p-5 bg-amber-300  rounded shadow-sm">
                             <div>
-                                <div class="text-sm text-gray-400 ">Users</div>
+                                <div class="font-black text-xl text-gray-900 ">Users</div>
                                 <div class="flex items-center pt-1">
-                                    <div class="text-3xl font-medium text-gray-600 "><?php echo $stat->countAdmin()['COUNT(id)'] ?></div>
+                                    <div class="text-3xl font-medium text-gray-50 "><?php echo $stat->countAdmin()['COUNT(id)'] ?></div>
                                 </div>
                             </div>
                             <div class="text-gray-500">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class=" w-9 h-9 text-gray-50">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
                                 </svg>
 
@@ -103,13 +103,13 @@ $stat = new Music();
 
                         <div class="flex items-center justify-between p-5 bg-amber-300  rounded shadow-sm">
                             <div>
-                                <div class="text-sm text-gray-400 ">Total Songs</div>
+                                <div class="font-black text-xl text-gray-900 ">Total Songs</div>
                                 <div class="flex items-center pt-1">
-                                    <div class="text-3xl font-medium text-gray-600 ">10</div>
+                                    <div class="text-3xl font-medium text-gray-50 ">10</div>
                                 </div>
                             </div>
                             <div class="text-gray-500 text-4xl">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9 text-gray-50">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                                 </svg>
 
